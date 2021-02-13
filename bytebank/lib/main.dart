1fe1b07329b426eda97fa9bb1e2916a9804a53d9
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-13 16:53:56 -0300

    Fundamentos - Aula 05.5 Ajustando o tema do projeto

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -5,9 +5,15 @@ void main() => runApp(BytebankApp());
 class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencias(),
+        theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary
+          )
         ),
+        home: ListaTransferencias(),
       );
 }
 
