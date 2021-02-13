2d2977896453781da31cbbc6d6d476c134892489
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-13 14:31:24 -0300

    Fundamentos - Aula 02.4 Implementando o design do item de transferencia

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -4,6 +4,24 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(title: Text("Transferencias")),
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("100.0"),
+                  subtitle: Text("1000"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("200.0"),
+                  subtitle: Text("1000"),
+                ),
+              ),
+            ],
+          ),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
           ),
