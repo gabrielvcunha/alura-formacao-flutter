cb1967d5259ee198fb266ae12aec2972e1ebb4c1
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-13 14:35:07 -0300

    Fundamentos - Aula 02.6 Refatorando o codigo

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -4,27 +4,43 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(title: Text("Transferencias")),
-          body: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("100.0"),
-                  subtitle: Text("1000"),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("200.0"),
-                  subtitle: Text("1000"),
-                ),
-              ),
-            ],
-          ),
+          body: ListaTransferencias(),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
           ),
         ),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Column(
+      children: <ItemTransferencia>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) => Card(
+    child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ),
+  );
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
