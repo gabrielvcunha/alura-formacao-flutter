9bcd1a7bf6e89ae38af2fdf9e188cd7e893a5176
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-14 19:57:32 -0300

    WebAPI - Aula 03.5 Lidando com problemas da comunicacao externa

--- bytebank/lib/screens/transactions_list.dart
+++ bytebank/lib/screens/transactions_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/centered_message.dart';
 import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/transaction.dart';
@@ -6,43 +7,53 @@ import 'package:flutter/material.dart';
 class TransactionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text('Transactions')),
-      body: FutureBuilder<List<Transaction>>(
-        future: findAll(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              break;
-            case ConnectionState.waiting:
-              return Progress(message: "Loading Transactions",);
-              break;
-            case ConnectionState.active:
-              break;
-            case ConnectionState.done:
-              final List<Transaction> transactions = snapshot.data;
-              return ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (context, index) {
-                  final Transaction transaction = transactions[index];
-                  return Card(
-                    child: ListTile(
-                      leading: Icon(Icons.monetization_on),
-                      title: Text(
-                        transaction.value.toString(),
-                        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        transaction.contact.accountNumber.toString(),
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ),
-                  );
-                },
-              );
-              break;
-          }
-          return Text("Unknown error");
-        },
-      ),
-    );
+        appBar: AppBar(title: Text('Transactions')),
+        body: FutureBuilder<List<Transaction>>(
+          future: findAll(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                return Progress(
+                  message: "Loading Transactions",
+                );
+                break;
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                if (snapshot.hasData) {
+                  final List<Transaction> transactions = snapshot.data;
+                  if (transactions.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount: transactions.length,
+                      itemBuilder: (context, index) {
+                        final Transaction transaction = transactions[index];
+                        return Card(
+                          child: ListTile(
+                            leading: Icon(Icons.monetization_on),
+                            title: Text(
+                              transaction.value.toString(),
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              transaction.contact.accountNumber.toString(),
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                }
+
+                return CenteredMessage("No transactions found",
+                    icon: Icons.warning);
+                break;
+            }
+            return CenteredMessage("Unknown error", icon: Icons.error);
+          },
+        ),
+      );
 }
