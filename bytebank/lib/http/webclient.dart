eaf2813bb85decf5a8c1e9038d8b1721a06b0c9c
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-14 19:09:26 -0300

    WebAPI - Aula 02.6 Utilizando o interceptador do http

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -1,6 +1,30 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print("Request");
+    print("method: ${data.method}");
+    print("url: ${data.url}");
+    print("headers: ${data.headers}");
+    print("body: ${data.body}");
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print("Response");
+    print("status: ${data.statusCode}");
+    print("headers: ${data.headers}");
+    print("body: ${data.body}");
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get("http://192.168.0.5:8080/transactions");
-  print(response.body);
+  final Client client =
+      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response =
+      await client.get("http://192.168.0.5:8080/transactions");
 }
