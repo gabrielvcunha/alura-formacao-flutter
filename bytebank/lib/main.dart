84bbd8149eea629f2b99b00358e97da9c4eeb84e
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-14 10:15:05 -0300

    Persistencia - Aula 03.6 Inserindo e buscando informacoes

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
+  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class BytebankApp extends StatelessWidget {
