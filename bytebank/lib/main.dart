ce89d434afc917bfe38b995851248ca83d904e17
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-13 22:40:30 -0300

    Persistencia - Aula 01.3 Criando o projeto

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,28 +1,14 @@
-import 'package:bytebank/models/saldo.dart';
-import 'package:bytebank/models/transferencias.dart';
-import 'package:bytebank/screens/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() => runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => Saldo(0)),
-        ChangeNotifierProvider(create: (context) => Transferencias()),
-      ],
-      child: BytebankApp(),
-    ));
+void main() {
+  runApp(BytebankApp());
+}
 
 class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.green[900],
-          accentColor: Colors.blueAccent[700],
-          buttonTheme: ButtonThemeData(
-            buttonColor: Colors.blueAccent[700],
-            textTheme: ButtonTextTheme.primary,
-          ),
-        ),
-        home: Dashboard(),
-      );
+    home: Scaffold(
+      appBar: AppBar(title: Text("Dashboard")),
+    ),
+  );
 }
