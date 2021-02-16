4ebc7ca09426225b9cf39d08b8de5e959e42a21d
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-16 19:09:15 -0300

    Testes - Aula 04.1 Verificando chamadas do banco de dados

--- bytebank/test/save_contact_flow.dart
+++ bytebank/test/save_contact_flow.dart
@@ -1,9 +1,11 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'matchers.dart';
 import 'mocks.dart';
@@ -12,7 +14,10 @@ void main() {
   testWidgets(
     "Should save a contact",
     (tester) async {
-      await tester.pumpWidget(BytebankApp(contactDAO: MockContactDao(),));
+      final MockContactDao mockContactDao = MockContactDao();
+      await tester.pumpWidget(BytebankApp(
+        contactDAO: mockContactDao,
+      ));
 
       final dashboard = find.byType(Dashboard);
       expect(dashboard, findsOneWidget);
@@ -27,6 +32,8 @@ void main() {
       final contactsList = find.byType(ContactsList);
       expect(contactsList, findsOneWidget);
 
+      verify(mockContactDao.findAll()).called(1);
+
       final newContactButton =
           find.widgetWithIcon(FloatingActionButton, Icons.add);
       expect(newContactButton, findsOneWidget);
@@ -37,25 +44,17 @@ void main() {
       final contactForm = find.byType(ContactForm);
       expect(contactForm, findsOneWidget);
 
-      final nameTextField = find.byWidgetPredicate((widget) {
-        if (widget is TextField) {
-          return widget.decoration.labelText == "Full name";
-        }
-        return false;
-      });
+      final nameTextField = find.byWidgetPredicate(
+          (widget) => _textFieldMatcher(widget, "Full name"));
       expect(nameTextField, findsOneWidget);
 
       await tester.enterText(nameTextField, "Eduardo");
 
-      final accountNumberTextField = find.byWidgetPredicate((widget) {
-        if (widget is TextField) {
-          return widget.decoration.labelText == "Account number";
-        }
-        return false;
-      });
+      final accountNumberTextField = find.byWidgetPredicate(
+          (widget) => _textFieldMatcher(widget, "Account number"));
       expect(accountNumberTextField, findsOneWidget);
 
-      await tester.enterText(nameTextField, "9999");
+      await tester.enterText(accountNumberTextField, "9999");
 
       final createButton = find.widgetWithText(RaisedButton, "Create");
       expect(createButton, findsOneWidget);
@@ -63,8 +62,19 @@ void main() {
       await tester.tap(createButton);
       await tester.pumpAndSettle();
 
+      verify(mockContactDao.save(Contact(0, "Eduardo", 9999)));
+
       final contactsListBack = find.byType(ContactsList);
       expect(contactsListBack, findsOneWidget);
+
+      verify(mockContactDao.findAll());
     },
   );
+}
+
+bool _textFieldMatcher(Widget widget, String labelText) {
+  if (widget is TextField) {
+    return widget.decoration.labelText == labelText;
+  }
+  return false;
 }
