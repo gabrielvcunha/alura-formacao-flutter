37600cebf2612cc1065ede5870d1f41899b10bc2
Gabriel Vargas <gabrielvcunha@gmail.com>
2021-02-16 19:35:09 -0300

    Testes - Aula 04.5 Adicionando comportamentos nos mocks

--- bytebank/test/flows/transfer_flow.dart
+++ bytebank/test/flows/transfer_flow.dart
@@ -1,6 +1,9 @@
+
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -19,6 +22,10 @@ void main() {
       final dashboard = find.byType(Dashboard);
       expect(dashboard, findsOneWidget);
 
+      when(mockContactDao.findAll()).thenAnswer((realInvocation) async {
+        return <Contact>[Contact(0, "Gabriel", 1000)];
+      });
+
       await clickOnTransferFeatureItem(tester);
       await tester.pumpAndSettle();
 
@@ -26,6 +33,23 @@ void main() {
       expect(contactsList, findsOneWidget);
 
       verify(mockContactDao.findAll()).called(1);
+
+      final contactItem = find.byWidgetPredicate(
+        (widget) {
+          if (widget is ContactItem) {
+            return widget.contact.name == "Gabriel" &&
+                widget.contact.accountNumber == 1000;
+          }
+          return false;
+        },
+      );
+      expect(contactItem, findsOneWidget);
+
+      await tester.tap(contactItem);
+      await tester.pumpAndSettle();
+
+      final transactionForm = find.byType(TransactionForm);
+      expect(transactionForm, findsOneWidget);
     },
   );
 }
